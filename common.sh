a127dbe0333988d1c2ab197d5607107ec8b476d8
Hetsh <git@hetsh.de>
2022-07-08 23:32:47 +0200

    Add POSIX parameter expansion cheat-sheet

--- common.sh
+++ common.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 
+# Cheat-Sheet for POSIX Parameter Expansion
+#
+# +--------------------+----------------------+-----------------+-----------------+
+# |   Expression       |       parameter      |     parameter   |    parameter    |
+# |   in script:       |   Set and Not Null   |   Set But Null  |      Unset      |
+# +--------------------+----------------------+-----------------+-----------------+
+# | ${parameter:-word} | substitute parameter | substitute word | substitute word |
+# | ${parameter-word}  | substitute parameter | substitute null | substitute word |
+# | ${parameter:=word} | substitute parameter | assign word     | assign word     |
+# | ${parameter=word}  | substitute parameter | substitute null | assign word     |
+# | ${parameter:?word} | substitute parameter | error, exit     | error, exit     |
+# | ${parameter?word}  | substitute parameter | substitute null | error, exit     |
+# | ${parameter:+word} | substitute word      | substitute null | substitute null |
+# | ${parameter+word}  | substitute word      | substitute word | substitute null |
+# +--------------------+----------------------+-----------------+-----------------+
+
 # Append snippet to a trap
 append_trap() {
 	local SIGNAL="$1"
