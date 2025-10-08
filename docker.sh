79c6d0573443f8c97d0a1950405b71754220a902
Hetsh <git@hetsh.de>
2025-10-08 09:10:15 +0200

    docker.sh: put escaped package name in variable PKG_ESCAPED in update_pkg_madison()

--- docker.sh
+++ docker.sh
@@ -114,13 +114,14 @@ update_pkg() {
 # Check for Debian package updates
 update_pkg_madison() {
 	local PKG && PKG="$1"
+	local PKG_ESCAPED && PKG_ESCAPED="${PKG//+/\\+}"
 	local NAME && NAME="$2"
 	local MAIN && MAIN="$3"
 	local URL && URL="$4"
 	local SUITE && SUITE="${5:-stable}"
 	local ARCH && ARCH="${6:-amd64}"
 
-	local CURRENT_VERSION && CURRENT_VERSION=$(grep --only-matching --perl-regexp "(?<=\s${PKG//+/\\+}=)[^\s]+" "Dockerfile")
+	local CURRENT_VERSION && CURRENT_VERSION=$(grep --only-matching --perl-regexp "(?<=\s$PKG_ESCAPED=)[^\s]+" "Dockerfile")
 	local NEW_VERSION && NEW_VERSION=$(curl --silent --location --data-urlencode "text=on" --data-urlencode "package=$PKG" --data-urlencode "a=$ARCH,all" --data-urlencode "s=$SUITE,$SUITE-update,$SUITE-security" "$URL" | tail -n 1 | tr -d '[:space:]' | cut -d '|' -f 2)
 	process_update "$PKG" "$NAME" "$MAIN" "$CURRENT_VERSION" "$NEW_VERSION"
 }
